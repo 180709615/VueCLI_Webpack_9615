a30e7736ec7d1683516cf39524b1bdfbc2408b19
180709615 <180709615@students.uajy.ac.id>
2020-11-10 22:58:39 +0700

    GD new

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -4,10 +4,10 @@
         <v-list-item>
             <v-list-item-content>
                 <v-list-item-title class="title">
-                    Nama Kalian
+                    Dimas Prayoga
                 </v-list-item-title>
                 <v-list-item-subtitle>
-                    NPM
+                    180709615
                 </v-list-item-subtitle>
             </v-list-item-content>
         </v-list-item>
